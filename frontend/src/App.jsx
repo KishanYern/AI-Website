0b89d8bbcda312f5e682d86ce5696b7ddb781462
Kishan <KishanYerneni@gmail.com>
2024-03-22 19:48:36 -0500

    added the 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router';
 import Home from './pages/home';
 import Laptop from './pages/laptopPage';
-import NavBar from './components/NavBar';
+import NotFound from './pages/notFound';
 
 function App() {
     return (
         <>
-            <NavBar />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/Laptop' element={<Laptop />} />
-                <Route path='*' element={<h1>404 Not Found</h1>} />
+                <Route path='*' element={<NotFound />} />
             </Routes>
         </>
     );
