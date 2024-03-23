f99466b3274040b32b3b1667ec64f4e510391be7
Kishan <KishanYerneni@gmail.com>
2024-03-22 19:49:06 -0500

    moved the navbar out of the app.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import HomeLanding from '../components/home/homeLand';
+import NavBar from '../components/NavBar'
 import homeAbout from '../components/home/homeAbout';
 
 const Home = () => {
     return (
-        <main className='h-full w-full pt-20'>
-            <HomeLanding />
+        <main className='h-full w-full'>
+            <NavBar />
+            <div className="pt-20">
+                <HomeLanding />
+            </div>
         </main>
     );
 };
