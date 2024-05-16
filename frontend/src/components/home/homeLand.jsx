476b5fabb48f1fdba468b9fdf84f84b58c25bfb1
Kishan Yerneni <kishanyerneni@gmail.com>
2024-05-16 16:56:19 -0500

    tinas work

--- frontend/src/components/home/homeLand.jsx
+++ frontend/src/components/home/homeLand.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { GiVintageRobot } from 'react-icons/gi';
 import background from '../../public/background.jpg';
 
 const homeLand = () => {
     return (
         <section
-            className='h-lvh bg-gradient-to-b from-slate-600 to-slate-200 flex justify-center 
-        items-center flex-col gap-10'
+            className='h-lvh flex justify-center 
+        items-center flex-row gap-10'
         >
-            <div className=' font-bold text-6xl font-amulya'>
-                Machine Learning Prediction Website
+            <div className='flex justify-center items-center flex-col gap-10'>
+                <div className=' font-bold text-6xl font-amulya'>
+                    Machine Learning Prediction Website
+                </div>
+                <div className='text-xl pb-24 font-synonym'>
+                    A website to make the machine learning models created
+                    available for public use and testing.
+                </div>
             </div>
-            <div className='text-xl pb-24 font-synonym'>
-                A website to make the machine learning models created available
-                for public use and testing.
+            <div className='flex justify-center items-center'>
+                <GiVintageRobot size={400} />
             </div>
         </section>
     );
