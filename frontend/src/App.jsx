39be799cac2cdcc4d9d337a07b9698d6054f0bfc
Kishan <KishanYerneni@gmail.com>
2024-10-05 23:36:37 -0500

    added model routes in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { Route, Routes } from 'react-router';
 import Home from './pages/home';
-import Laptop from './pages/laptopPage';
+import Diabeties from './components/models/diabeties';
+import Embolism from './components/models/embolism';
+import Laptop from './components/models/laptop';
+import Lung from './components/models/lung';
+import Medical from './components/models/medical'
 import NotFound from './pages/notFound';
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/Laptop' element={<Laptop />} />
+                <Route path='/Diabeties' element={<Diabeties />} />
+                <Route path='/Embolism' element={<Embolism />} />
+                <Route path='/Lung' element={<Lung />} />
+                <Route path='/Medical' element={<Medical />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
         </>
---
+++ frontend/src/components/models/diabeties.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const diabeties = () => {
+  return (
+    <div>diabeties</div>
+  )
+}
+
+export default diabeties
