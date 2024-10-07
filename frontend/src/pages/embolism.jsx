1f46f587566b28a65ebada7e3bce6dd17f461a86
Kishan <KishanYerneni@gmail.com>
2024-10-07 17:27:57 -0500

    added path back to home for each of the model pages

--- frontend/src/pages/embolism.jsx
+++ frontend/src/pages/embolism.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const embolism = () => {
   return (
-    <div>embolism</div>
+    <>
+      <div> embolism </div>
+      <div className="flex items-center justify-center">
+        <Link className='font-satoshi text-white text-3xl flex items-center justify-center mt-4 bg-slate-800 hover:bg-slate-500 duration-100 rounded-lg p-3' to={'/'}>
+          Click here to go back to the home page
+        </Link>
+      </div>
+    </>
   )
 }
 
