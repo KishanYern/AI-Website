ee909d562e543ccbae5808e6a62a3ba92b99555e
Kishan Yerneni <kishanyerneni@gmail.com>
2024-10-15 13:02:40 -0500

    fixed key console warning

--- frontend/src/components/home/homeAbout.jsx
+++ frontend/src/components/home/homeAbout.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import aboutSection from '../../../public/aboutData';
 
-const HomeAbout = () => {
+const homeAbout = () => {
     return (
-        <div className="bg-gradient-to-b from-slate-200 to-blue-100 flex justify-center 
-            items-center flex-col gap-10 pl-32 pr-32 pb-56">
-            <div className="text-5xl pb-10">
-                About This Website!
-            </div>
+        <div
+            className='bg-gradient-to-b from-slate-200 to-blue-100 flex justify-center 
+        items-center flex-col gap-10 pl-32 pr-32 pb-56'
+        >
+            <div className=' text-5xl pb-10'>About This Website!</div>
             <div>
-                {aboutSection.map((paragraph, index) => (
-                    <div 
-                        key={index} 
-                        className="text-xl pb-10 font-trenchSlab flex justify-center items-center"
-                    >
-                        {paragraph}
-                    </div>
-                ))}
+                {aboutSection.map((paragraph) => {
+                    return (
+                        <div
+                            key={paragraph.key}
+                            className='text-xl pb-10 font-trenchSlab flex justify-center items-center'
+                        >
+                            {' '}
+                            {paragraph.data}{' '}
+                        </div>
+                    );
+                })}
             </div>
         </div>
+    );
     );
 };
 
--- frontend/src/components/home/homeModels.jsx
+++ frontend/src/components/home/homeModels.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import CardData from '../../../public/modelData'
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import CardData from '../../../public/modelData';
 
 const homeModels = () => {
-  return (
-    <div className='bg-gradient-to-b from-blue-100 to-red-200 flex justify-center 
-        items-center flex-col gap-10 pb-56'>
-        <div className=' text-5xl pb-10'>
-            Models
-        </div>
-        <div className='flex justify-center items-center gap-10 flex-row flex-wrap'>
-            {CardData.map( card => {
-                return <Link key={card.key} to={card.link}>
-                    <div id={card.key} key={card.key} className='text-xl text-black rounded-xl p-2 h-auto md:h-5/6 w-auto cursor-pointer
+    return (
+        <div
+            className='bg-gradient-to-b from-blue-100 to-red-200 flex justify-center 
+        items-center flex-col gap-10 pb-56'
+        >
+            <div className=' text-5xl pb-10'>Models</div>
+            <div className='flex justify-center items-center gap-10 flex-row flex-wrap'>
+                {CardData.map((card) => {
+                    return (
+                        <Link to={card.link} key={card.key}>
+                            <div
+                                key={card.key}
+                                className='text-xl text-black rounded-xl p-2 h-auto md:h-5/6 w-auto cursor-pointer
                         shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] hover:scale-105 
-                        hover:shadow-2xl duration-200 bg-gradient-to-br from-red-100 to-red-50 font-projects'>
-                        {card.title}
-                    </div>
-                </Link>
-            })}
+                        hover:shadow-2xl duration-200 bg-gradient-to-br from-red-100 to-red-50 font-projects'
+                            >
+                                {card.title}
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default homeModels
+export default homeModels;
