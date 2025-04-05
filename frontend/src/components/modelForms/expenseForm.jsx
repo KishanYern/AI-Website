8348b2b38230cec2e66a339045643ed9bf6d1016
Kishan <Kishanyerneni@gmail.com>
2025-04-05 13:46:07 -0500

    changed the tooltip color to better contrast the background

--- frontend/src/components/modelForms/expenseForm.jsx
+++ frontend/src/components/modelForms/expenseForm.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const embolismForm = () => {
     return (
-        <div>embolismForm</div>
+        <div>
+             <Tooltip
+                message={{
+                    title: 'Before You Start',
+                    text: 'Please note that this model should not replace any professional appraisal. The results generated are based on our learning and may not be 100% accurate. Always consult a \
+                qualified professional for precise evaluations. Also note that the data used to train these models are from India, and consist female patients of at least 21 years old and of Pima Indian heritage.',
+                }}
+            >
+                <HiOutlineQuestionMarkCircle
+                    className='absolute top-2 right-8'
+                    size={30}
+                />
+            </Tooltip>
+            
+        </div>
     )
 }
 
--- frontend/src/components/modelForms/tooltip.jsx
+++ frontend/src/components/modelForms/tooltip.jsx
@@ -1,11 +1,12 @@
 export default function Tooltip({ message, children }) {
     return (
-    <div class="group relative flex">
-        {children}
-        <div className="scale-0 transition-all rounded gap-0 bg-blue-400 p-2 text-white group-hover:scale-100 absolute top-0 flex items-center justify-center flex-col">
-            <span class="text-2xl font-mate">{message.title}</span> <br />
-            <span class="text-base font-mate pb-7">{message.text}</span>
+        <div class="group relative flex">
+            {children}
+            <div className="scale-0 transition-all rounded gap-0 bg-gray-800 p-2 text-white group-hover:scale-100 absolute top-0 flex items-center justify-center flex-col opacity-95 shadow-lg border border-gray-600">
+                <span class="text-2xl font-mate text-blue-300">{message.title}</span> <br />
+                <span class="text-base font-mate pb-7 text-gray-200">{message.text}</span>
+                <div className="absolute -bottom-1 w-4 h-4 bg-gray-800 transform rotate-45 border-b border-r border-gray-600"></div>
+            </div>
         </div>
-    </div>
     )
 }
